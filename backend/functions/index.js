d812d998f5ab140bb45bbba5a2120512bbc68f41
bigguysahaj <singhsahaj2001@gmail.com>
2023-05-10 06:02:08 +0530

    backend complete

--- backend/functions/index.js
+++ backend/functions/index.js
@@ -1,17 +1,33 @@
 const functions = require("firebase-functions");
-
-const scraper = require("./functions/scraper");
+const fs = require("fs/promises");
+const scraper = require("./scraper");
 const admin = require("firebase-admin");
 
 admin.initializeApp();
+const db = admin.firestore();
+
+// use this when you need date for the leaderboard
+// const getToday = () => {
+//   const today = new Date();
+//   return `${today.getMonth() + 1}/${today.getDate()}/${today.getFullYear()}`;
+// };
+
+const usernames = async () => {
+  const users = await fs.readFile("students.json", "utf-8");
+  return JSON.parse(users);
+};
 
 exports.pubsub = functions
+    .region("us-central1")
+    .runWith({memory: "2GB"})
     .pubsub.schedule("0 0 * * *")
-    .timezone("Asia/Kolkata")
+    .timeZone("America/New_York")
     .onRun(async () => {
       try {
-        await scraper.scrapeData();
+        const users = await usernames();
+        const scrapedData = await scraper.scrapeData(users);
+        await db.collection("leaderboard").doc("board").set({scrapedData});
       } catch (error) {
         throw new Error(error);
       }
-    })
+    });
--- backend/functions/scraper.js
+++ backend/functions/scraper.js
@@ -1,42 +1,32 @@
-const puppeteer = require('puppeteer');
-const functions = require("firebase-functions");
-const fs = require("fs/promises");
-const cors = require("cors")({origin: true});
+const puppeteer = require("puppeteer");
 
 const scrapeData = async (users) => {
-    const browser = await puppeteer.launch({
-      headless: true
-    });
-    const result = [];
+  const browser = await puppeteer.launch({
+    headless: true,
+  });
+  const result = [];
 
-    for (const user of users) {
-      const page = await browser.newPage();
-      await page.goto(`https://leetcode.com/${user.username}/`);
+  for (const user of users) {
+    const page = await browser.newPage();
+    await page.goto(`https://leetcode.com/${user.username}/`);
 
-      const scores = await page.$$eval(
-          "div[class=\"text-[24px] font-medium text-label-1 dark:text-dark-label-1\"]", // eslint-disable-line max-len
-          (elements) => elements.map((e) => e.innerText),
-      );
+    const scores = await page.$$eval(
+        "div[class=\"text-[24px] font-medium text-label-1 dark:text-dark-label-1\"]", // eslint-disable-line max-len
+        (elements) => elements.map((e) => e.innerText),
+    );
 
-      await page.close();
-
-      result.push({name: user.name, score: scores[0]});
+    await page.close();
+    if (scores[0] === undefined) {
+      result.push({name: user.name, score: 0});
+      continue;
     }
 
-    await browser.close();
+    result.push({name: user.name, score: parseInt(scores[0])});
+  }
 
-    return result;
+  await browser.close();
+
+  return result;
 };
 
-const usernames = async () => {
-  const users = await fs.readFile("students.json", "utf-8");
-  return JSON.parse(users);
-};
-
-usernames().then((users) => {
-  scrapeData(users).then((res) => {
-    console.log(res);
-  });
-});
-
-exports.scraper = scrapeData;
+exports.scrapeData = scrapeData;
